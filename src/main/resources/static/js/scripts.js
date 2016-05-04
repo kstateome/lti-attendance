c5eb86700f28fad749b0f19c7f27422d05d2e554
Jake Macek <jmacek@ksu.edu>
2016-05-04 16:39:27 -0500

    Got selected sections working

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,9 @@
 function toggleSection($val) {
-    console.log($val);
+    var elements = document.getElementsByClassName('hideme');
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+
+    document.getElementById($val).style.display = "block";
 }
