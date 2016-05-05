076a4dce052d02e0641a8e2b70dd4d723fb5f3f7
Kurt Zoglmann <kaz0358@ksu.edu>
2016-05-05 11:13:19 -0500

    Trivial. Corrected Logger usage

--- src/main/java/edu/ksu/canvas/aviation/util/RoleChecker.java
+++ src/main/java/edu/ksu/canvas/aviation/util/RoleChecker.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 import java.util.List;
 
 public class RoleChecker {
-    private static Logger LOG = Logger.getLogger(RoleChecker.class);
+    private static final Logger LOG = Logger.getLogger(RoleChecker.class);
     private final List<LtiLaunchData.InstitutionRole> validRoles;
 
     public RoleChecker(List<LtiLaunchData.InstitutionRole> validRoles) {
