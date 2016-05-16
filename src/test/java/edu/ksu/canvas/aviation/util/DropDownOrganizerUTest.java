19d7f64da65187550a5a7d38f30442f72a870399
Killian Stone <killsto@ksu.edu>
2016-05-16 10:16:20 -0500

    Fixed tests

--- src/test/java/edu/ksu/canvas/aviation/util/DropDownOrganizerUTest.java
+++ src/test/java/edu/ksu/canvas/aviation/util/DropDownOrganizerUTest.java
@@ -1,6 +1,6 @@
 package edu.ksu.canvas.aviation.util;
 
-import edu.ksu.canvas.model.Section;
+import edu.ksu.canvas.aviation.entity.AviationSection;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,51 +13,52 @@ public class DropDownOrganizerUTest {
     private static long sectionId1 = 1;
     private static long sectionId2 = 2;
     private static long sectionId3 = 3;
-    private List<Section> preSortedSections = new ArrayList<>();
-    private List<Section> unSortedSections = new ArrayList<>();
-    private Section section1 = new Section();
-    private Section section2 = new Section();
-    private Section section3 = new Section();
+    private List<AviationSection> preSortedAviationSections = new ArrayList<>();
+    private List<AviationSection> unSortedAviationSections = new ArrayList<>();
+    private AviationSection section1 = new AviationSection();
+    private AviationSection section2 = new AviationSection();
+    private AviationSection section3 = new AviationSection();
 
     public DropDownOrganizerUTest() {
-        section1.setId(sectionId1);
+        section1.setCanvasSectionId(sectionId1);
         section1.setName("A");
-        section2.setId(sectionId2);
+        section2.setCanvasSectionId(sectionId2);
         section2.setName("B");
-        section3.setId(sectionId3);
+        section3.setCanvasSectionId(sectionId3);
         section3.setName("C");
     }
-    @Before
-    public void setupPresortedSections() {
-        preSortedSections = Arrays.asList(section1, section2, section3);
-    }
 
     @Before
-    public void setupUnSortedSections() {
-        unSortedSections = Arrays.asList(section3, section1, section2);
+    public void setupPresortedAviationSections() {
+        preSortedAviationSections = Arrays.asList(section1, section2, section3);
+    }
+
+    @Before
+    public void setupUnSortedAviationSections() {
+        unSortedAviationSections = Arrays.asList(section3, section1, section2);
     }
 
     @Test
-    public void sortsWithNullSelectedSectionId() {
-        List<Section> sortedSections = DropDownOrganizer.sortWithSelectedSectionFirst(unSortedSections, null);
-        Assert.assertEquals("Expected sorted preSortedSections to match presortedSections", preSortedSections, sortedSections);
+    public void sortsWithNullSelectedAviationSectionId() {
+        List<AviationSection> sortedAviationSections = DropDownOrganizer.sortWithSelectedSectionFirst(unSortedAviationSections, null);
+        Assert.assertEquals("Expected sorted preSortedAviationSections to match presortedAviationSections", preSortedAviationSections, sortedAviationSections);
     }
 
     @Test
-    public void putsSelectedSectionInFirstPosition() {
-        long selectedSectionId = sectionId3;
-        List<Section> sortedSections = DropDownOrganizer.sortWithSelectedSectionFirst(unSortedSections, String.valueOf(selectedSectionId));
-        long index0SectionId = sortedSections.get(0).getId();
-        Assert.assertEquals("Expected selected section to be at index 0 after sorted", selectedSectionId, index0SectionId);
+    public void putsSelectedAviationSectionInFirstPosition() {
+        long selectedAviationSectionId = sectionId3;
+        List<AviationSection> sortedAviationSections = DropDownOrganizer.sortWithSelectedSectionFirst(unSortedAviationSections, String.valueOf(selectedAviationSectionId));
+        long index0AviationSectionId = sortedAviationSections.get(0).getCanvasSectionId();
+        Assert.assertEquals("Expected selected section to be at index 0 after sorted", selectedAviationSectionId, index0AviationSectionId);
     }
 
     @Test
-    public void restOfListIsSortedWhenSectionIsSelected() {
-        long selectedSectionId = sectionId3;
-        List<Section> sortedSections = DropDownOrganizer.sortWithSelectedSectionFirst(unSortedSections, String.valueOf(selectedSectionId));
-        long index1SectionId = sortedSections.get(1).getId();
-        long index2SectionId = sortedSections.get(2).getId();
-        Assert.assertEquals("Expected section 1 to be at 2nd position of list", sectionId1, index1SectionId);
-        Assert.assertEquals("Expected section 2 to be at 3rd position of list", sectionId2, index2SectionId);
+    public void restOfListIsSortedWhenAviationSectionIsSelected() {
+        long selectedAviationSectionId = sectionId3;
+        List<AviationSection> sortedAviationSections = DropDownOrganizer.sortWithSelectedSectionFirst(unSortedAviationSections, String.valueOf(selectedAviationSectionId));
+        long index1AviationSectionId = sortedAviationSections.get(1).getCanvasSectionId();
+        long index2AviationSectionId = sortedAviationSections.get(2).getCanvasSectionId();
+        Assert.assertEquals("Expected section 1 to be at 2nd position of list", sectionId1, index1AviationSectionId);
+        Assert.assertEquals("Expected section 2 to be at 3rd position of list", sectionId2, index2AviationSectionId);
     }
 }
