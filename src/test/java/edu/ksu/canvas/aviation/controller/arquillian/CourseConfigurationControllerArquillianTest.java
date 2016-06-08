bfb63cb598fe6689dc04fd895b867225e762790d
alexanda <alexanda@k-state.edu>
2016-06-08 09:46:54 -0500

    Refactored Roster tests into appropriately named class

--- src/test/java/edu/ksu/canvas/aviation/controller/arquillian/CourseConfigurationControllerArquillianTest.java
+++ src/test/java/edu/ksu/canvas/aviation/controller/arquillian/CourseConfigurationControllerArquillianTest.java
@@ -2,19 +2,9 @@ package edu.ksu.canvas.aviation.controller.arquillian;
 
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.arquillian.warp.Activity;
-import org.jboss.arquillian.warp.Inspection;
-import org.jboss.arquillian.warp.Warp;
 import org.jboss.arquillian.warp.WarpTest;
-import org.jboss.arquillian.warp.client.filter.http.HttpFilters;
-import org.jboss.arquillian.warp.servlet.AfterServlet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.springframework.web.servlet.ModelAndView;
-
-import static org.junit.Assert.assertEquals;
 
 
 @WarpTest
@@ -23,37 +13,8 @@ import static org.junit.Assert.assertEquals;
 public class CourseConfigurationControllerArquillianTest extends BaseArquillianTest{
 
     @Test
-    public void shouldBeAbleToInject() throws Exception {
-        System.out.println("first test.. using driver: "+driver+"  .. fetching this url: " + baseUrl + "roster");
-
-        driver.navigate().to(baseUrl + "roster");
-        System.out.println("Page source: \n" + driver.getPageSource());
-
-        Warp.initiate(new Activity() {
-            
-            @Override
-            public void perform() {
-                System.err.println("Submitting roster form");
-                driver.findElement(By.id("saveAttendanceOnTop")).click();
-            }
-        }).observe(HttpFilters.request().uri().contains("roster"))
-        .inspect(new Inspection() {
-            private static final long serialVersionUID = 1L;
-            
-            @ArquillianResource
-            private ModelAndView modelAndView; 
-            
-            @AfterServlet
-            public void testAfterServlet() {
-                //cannot use logger above
-                System.out.println("modelAndView after save: "+modelAndView);
-                
-                Boolean saveSuccessExpected = Boolean.TRUE;
-                Boolean saveSuccessActual = (Boolean) modelAndView.getModel().get("saveSuccess");
-                assertEquals(saveSuccessExpected, saveSuccessActual);
-            }
-        });
-
+    public void shouldBeAbleToSaveConfiguration() throws Exception {
+        //TODO: actually write a test for this 
     }
     
 }
---
+++ src/test/java/edu/ksu/canvas/aviation/controller/arquillian/RosterControllerArquillianTest.java
@@ -0,0 +1,56 @@
+package edu.ksu.canvas.aviation.controller.arquillian;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.warp.Activity;
+import org.jboss.arquillian.warp.Inspection;
+import org.jboss.arquillian.warp.Warp;
+import org.jboss.arquillian.warp.WarpTest;
+import org.jboss.arquillian.warp.client.filter.http.HttpFilters;
+import org.jboss.arquillian.warp.servlet.AfterServlet;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.assertEquals;
+
+@WarpTest
+@RunAsClient
+@RunWith(Arquillian.class)
+public class RosterControllerArquillianTest extends BaseArquillianTest {
+
+    @Test
+    public void shouldBeAbleToSave() throws Exception {
+        System.out.println("Testing Roster page.. using driver: "+driver+"  .. fetching this url: " + baseUrl + "roster");
+
+        driver.navigate().to(baseUrl + "roster");
+        System.out.println("Page source: \n" + driver.getPageSource());
+
+        Warp.initiate(new Activity() {
+
+            @Override
+            public void perform() {
+                System.err.println("Submitting roster form");
+                driver.findElement(By.id("saveAttendanceOnTop")).click();
+            }
+        }).observe(HttpFilters.request().uri().contains("roster"))
+                .inspect(new Inspection() {
+                    private static final long serialVersionUID = 1L;
+
+                    @ArquillianResource
+                    private ModelAndView modelAndView;
+
+                    @AfterServlet
+                    public void testAfterServlet() {
+                        //cannot use logger above
+                        System.out.println("modelAndView after save: " + modelAndView);
+
+                        Boolean saveSuccessExpected = Boolean.TRUE;
+                        Boolean saveSuccessActual = (Boolean) modelAndView.getModel().get("saveSuccess");
+                        assertEquals(saveSuccessExpected, saveSuccessActual);
+                    }
+                });
+    }
+}
