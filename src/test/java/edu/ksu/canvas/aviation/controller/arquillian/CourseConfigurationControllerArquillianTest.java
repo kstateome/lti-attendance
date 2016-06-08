94011ac0bc12f01dd6abf616315257b38a7a0864
alexanda <alexanda@k-state.edu>
2016-06-08 14:47:14 -0500

    Added happy path test for saving the course configuration

--- src/test/java/edu/ksu/canvas/aviation/controller/arquillian/CourseConfigurationControllerArquillianTest.java
+++ src/test/java/edu/ksu/canvas/aviation/controller/arquillian/CourseConfigurationControllerArquillianTest.java
@@ -2,9 +2,20 @@ package edu.ksu.canvas.aviation.controller.arquillian;
 
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.warp.Activity;
+import org.jboss.arquillian.warp.Inspection;
+import org.jboss.arquillian.warp.Warp;
 import org.jboss.arquillian.warp.WarpTest;
+import org.jboss.arquillian.warp.client.filter.http.HttpFilters;
+import org.jboss.arquillian.warp.client.filter.http.HttpMethod;
+import org.jboss.arquillian.warp.servlet.AfterServlet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.assertEquals;
 
 
 @WarpTest
@@ -14,7 +25,33 @@ public class CourseConfigurationControllerArquillianTest extends BaseArquillianT
 
     @Test
     public void shouldBeAbleToSaveConfiguration() throws Exception {
-        //TODO: actually write a test for this 
+        System.out.println("Testing config page.. using driver: "+driver+"  .. fetching this url: " + baseUrl + "courseConfiguration");
+
+        driver.navigate().to(baseUrl + "courseConfiguration");
+        driver.findElement(By.id("classSetupLink")).click();
+        System.out.println("Page source: \n" + driver.getPageSource());
+
+        Warp.initiate(new Activity() {
+            @Override
+            public void perform() {
+                System.err.println("Saving configuration");
+                driver.findElement(By.id("saveCourseConfiguration")).click();
+            }
+        }).observe(HttpFilters.request().method().equal(HttpMethod.POST))
+                .inspect(new Inspection() {
+                    private static final long serialVersionUID = 2L;
+
+                    @ArquillianResource
+                    private ModelAndView modelAndView;
+
+                    @AfterServlet
+                    public void testAfterServlet() {
+                        //cannot use logger above
+                        System.out.println("modelAndView after save: " + modelAndView);
+                        Boolean updateSuccessful = (Boolean) modelAndView.getModel().get("updateSuccessful");
+                        assertEquals(true, updateSuccessful);
+                    }
+                });
     }
     
 }
