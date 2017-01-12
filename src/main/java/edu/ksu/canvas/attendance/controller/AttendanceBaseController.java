8f6789616c9b367f17e01dc4fc4030e41c5ff3f0
Jake Macek <jmacek@ksu.edu>
2017-01-12 12:43:09 -0600

    Removed unused import

--- src/main/java/edu/ksu/canvas/attendance/controller/AttendanceBaseController.java
+++ src/main/java/edu/ksu/canvas/attendance/controller/AttendanceBaseController.java
@@ -22,7 +22,6 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Controller
