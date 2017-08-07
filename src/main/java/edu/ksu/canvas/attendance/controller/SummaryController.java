55978495457f16d80e393b53d76053ee4b95c19c
reish981 <reish@ksu.edu>
2017-08-07 11:05:43 -0500

    Revert "Merge branch 'test1' into merge-41E"
    This reverts commit 40a471c0ac45a805dbdafe5f54eef465a51e737f, reversing
    changes made to 8620fbcb7c670db128a56dcf420106212fdea548.

--- src/main/java/edu/ksu/canvas/attendance/controller/SummaryController.java
+++ src/main/java/edu/ksu/canvas/attendance/controller/SummaryController.java
@@ -21,6 +21,9 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import edu.ksu.canvas.attendance.util.DropDownOrganizer;
+import java.util.ArrayList;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
