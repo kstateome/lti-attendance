cce9353d20ce7771d003a955abdd0d63490221f1
Killian Stone <killsto@ksu.edu>
2017-09-11 15:00:19 -0500

    Remove unused variable

--- src/test/java/edu/ksu/canvas/attendance/controller/arquillian/page/RosterPage.java
+++ src/test/java/edu/ksu/canvas/attendance/controller/arquillian/page/RosterPage.java
@@ -3,17 +3,13 @@ package edu.ksu.canvas.attendance.controller.arquillian.page;
 import edu.ksu.canvas.attendance.controller.arquillian.page.fragments.DateTableFragment;
 import edu.ksu.canvas.attendance.controller.arquillian.page.fragments.SectionSelectDropDown;
 import edu.ksu.canvas.attendance.controller.arquillian.page.fragments.StudentRoster;
-import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.page.Location;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 @Location("roster")
 public class RosterPage {
-    @Drone
-    private WebDriver browser;
 
     @FindBy(id = "sectionSelectFormGroup")
     private SectionSelectDropDown sectionSelectDropDown;
