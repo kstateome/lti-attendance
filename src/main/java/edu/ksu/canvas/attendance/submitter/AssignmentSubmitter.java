0434d5bc456e01a05bc33f7a426d61b3096322e7
wolfejar <wolfejar@ksu.edu>
2018-05-17 14:17:04 -0500

    removed comments

--- src/main/java/edu/ksu/canvas/attendance/submitter/AssignmentSubmitter.java
+++ src/main/java/edu/ksu/canvas/attendance/submitter/AssignmentSubmitter.java
@@ -147,11 +147,9 @@ public class AssignmentSubmitter {
             returnedProgress = canvasApiWrapperService.gradeMultipleSubmissionsByCourse(oauthToken, submissionOptions);
         } catch (IOException e) {
             LOG.error("Error while pushing the grades of course: " + courseId, e);
-            //throw new AttendanceAssignmentException(AttendanceAssignmentException.Error.FAILED_PUSH);
         }
         if (!isValidProgress(returnedProgress)) {
             LOG.error("Error object returned while pushing the grades of course: " + courseId);
-            //throw new AttendanceAssignmentException(AttendanceAssignmentException.Error.FAILED_PUSH);
         }
     }
 
